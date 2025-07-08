9f37a4583232d1fbea2bf666b65e9ee43e7c5281
cdandeniya <chanul.dandeniya@stonybrook.edu>
2025-07-07 22:06:06 -0400

    Regenerate package-lock.json and sync all changes

--- mapai/metro.config.js
+++ mapai/metro.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  resolver: {
-    assetExts: ['db', 'mp3', 'ttf', 'obj', 'png', 'jpg', 'jpeg', 'bin', 'mp4', 'wav', 'css'],
-  },
-}; 
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Add 'css' to assetExts for Mapbox GL web support
+config.resolver.assetExts.push('css');
+
+module.exports = config; 
